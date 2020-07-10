f39f41862405cf690165329e4c2639b582aaf1b1
Jesús Hernández <jesus.hernandezmoreno@outlook.com>
2020-07-10 01:45:51 -0500

    Updates

---
+++ precache-manifest.15776a55bdce92d67dd98b652dbe8686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807be17f83512f9a65d4ef8eed2179ee",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "962b5574734c76f7f539",
+    "url": "/hacker-news-clone/static/css/main.2d094360.chunk.css"
+  },
+  {
+    "revision": "908afcef8b36674049f2",
+    "url": "/hacker-news-clone/static/js/2.9aa7162d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/hacker-news-clone/static/js/2.9aa7162d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962b5574734c76f7f539",
+    "url": "/hacker-news-clone/static/js/main.2e0cbd0f.chunk.js"
+  },
+  {
+    "revision": "494647f775d812ee3fce",
+    "url": "/hacker-news-clone/static/js/runtime-main.1eb8d402.js"
+  }
+]);
